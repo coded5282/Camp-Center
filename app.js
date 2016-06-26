e8da3870d437506446736aaa737867ad3b1d5428
Edward Chen <edwardjchen@gmail.com>
2016-06-26 19:11:51 +0000

    added data persistence

--- app.js
+++ app.js
@@ -1,35 +1,62 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser"); 
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"); 
 
-
+mongoose.connect("mongodb://localhost/yelp_camp"); 
 app.use(bodyParser.urlencoded({extended: true})); 
 app.set("view engine", "ejs"); 
 
- var campgrounds = [
-        {name: "Salmon Creek", image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5253636.jpg"},    
-        {name: "Granite Creek", image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5253636.jpg"},    
-        {name: "Granite Creek", image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5253636.jpg"},    
-        {name: "Granite Creek", image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5253636.jpg"},    
-        {name: "Granite Creek", image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5253636.jpg"},    
-        {name: "Granite Creek", image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5253636.jpg"},    
-        {name: "Red Creek", image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5253636.jpg"}    
-    ];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String 
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema); 
+
+// Campground.create({
+//     name: "Granite Creek", 
+//     image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5253636.jpg"
+    
+//     }, function(err, campground) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("NEW");
+//             console.log(campground); 
+//         }
+//     }); 
 
 app.get("/", function(req, res) {
     res.render("landing"); 
 }); 
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds:campgrounds}); 
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds) {
+       if (err) {
+           console.log(err);
+       } else {
+            res.render("campgrounds", {campgrounds:allCampgrounds}); 
+       }
+    });
+    // res.render("campgrounds", {campgrounds:campgrounds}); 
 });
 
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image; 
     var newCampground = {name: name, image: image}; 
-    campgrounds.push(newCampground)
-    res.redirect("/campgrounds"); 
+    // Create a new campground and save to DB 
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err); 
+        } else {
+            res.redirect("/campgrounds"); 
+        }
+    });
+    
    // get data from form and add to campgrounds array
    // redirect back to campgrounds page 
 });
